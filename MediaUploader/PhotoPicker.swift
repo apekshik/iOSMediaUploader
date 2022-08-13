e3d6f91a85387e3df1ea79005ba8ae49ef89fc2b
Apekshik Panigrahi <46071272+apekshik@users.noreply.github.com>
2022-08-13 15:41:56 +0530

    Added photo manager to extract image from selectedItem and store it in Data.

--- MediaUploader/PhotoPicker.swift
+++ MediaUploader/PhotoPicker.swift
@@ -12,29 +12,50 @@ struct PhotoPicker: View {
     
     @State private var selectedItem: PhotosPickerItem? = nil
     @State private var selectedImageData: Data? = nil
+    @State private var offset = CGSize.zero
     
     var body: some View {
-        PhotosPicker(
-            selection: $selectedItem,
-            matching: .images,
-            photoLibrary: .shared()) {
-                Text("Select a photo")
-            }
-            .onChange(of: selectedItem) { newItem in
-                Task {
-                    // Retrive selected asset in the form of Data
-                    if let data = try? await newItem?.loadTransferable(type: Data.self) {
-                        selectedImageData = data
+        VStack {
+            PhotosPicker(
+                selection: $selectedItem,
+                matching: .images,
+                photoLibrary: .shared()) {
+                    Text("Select a photo")
+                }
+                .onChange(of: selectedItem) { newItem in
+                    Task {
+                        // Retrive selected asset in the form of Data
+                        if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                            selectedImageData = data
+                        }
                     }
                 }
+            if let selectedImageData,
+               let image = UIImage(data: selectedImageData) {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250)
+                    .cornerRadius(20)
+                    .rotation3DEffect(.degrees(4), axis: (x: 0, y: 1, z: 0))
+                    .offset(x: offset.width, y: 0)
+                    .opacity(2 - Double(abs(offset.width / 50)))
+                    .gesture(
+                        DragGesture()
+                            .onChanged { gesture in
+                                offset = gesture.translation
+                            }
+                            .onEnded { _ in
+                                if abs(offset.width) > 200 {
+                                    // remove the card
+                                } else {
+                                    offset = .zero
+                                }
+                            }
+                    )
             }
-        if let selectedImageData,
-           let image = UIImage(data: selectedImageData) {
-            Image(uiImage: image)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 250, height: 250)
         }
+        
     }
 }
 
