537447c9efe872fe2972dd73cc10bdd94dae2265
Apekshik Panigrahi <46071272+apekshik@users.noreply.github.com>
2022-08-13 14:57:52 +0530

    Initial Commit

---
+++ MediaUploader/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MediaUploader
+//
+//  Created by Apekshik Panigrahi on 8/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MediaUploader/MediaUploaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaUploaderApp.swift
+//  MediaUploader
+//
+//  Created by Apekshik Panigrahi on 8/13/22.
+//
+
+import SwiftUI
+
+@main
+struct MediaUploaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
